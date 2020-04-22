3b8b61395f3f9060f3a3cee73a26feb9cad03200
Joel <joel28leiva@gmail.com>
2020-04-22 00:29:57 -0500

    Basico - parte 11

--- src/com/fabrica/basico/Ejemplo1.java
+++ src/com/fabrica/basico/Ejemplo1.java
@@ -1,4 +1,28 @@
 package com.fabrica.basico;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Ejemplo1 {
+    public static void main(String[] args) {
+        //instaciar un objeto
+        WebDriver driver;
+
+        //declarar variables
+        String baseUrl = "http://newtours.demoaut.com/";
+        String actualResult="";
+        String expectedResult="Welcome: Mercury Tours";
+
+        //Indicar la localizacion del archivo chromedriver.exe
+        //donde se encuentra nuestro archivo
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\driver\\chromedriver.exe");
+
+        //Abrir el navegador chrome
+        driver = new ChromeDriver();
+
+
+    }
+
+
+
 }
